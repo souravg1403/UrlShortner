51e04764daa4a9c5d07b5e8517911b85a5bb7fb5
Sourav Gupta <souravmusic84@gmail.com>
2023-11-03 08:44:53 +0530

    Added helper file

--- helper.go
+++ helper.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"errors"
+
 	"github.com/redis/go-redis/v9"
 )
 
 type RedisConnection struct {
-	RedisClient *redisClient
+	RedisClient *redis.Client
 }
 
 func RedisConnectionHandler() RedisConnection {
@@ -20,16 +22,38 @@ func RedisConnectionHandler() RedisConnection {
 		RedisClient: connection,
 	}
 
-	redis
+	return handler
 }
 
-func (r RedisConnection) setKey(key, value string) {
+func (r RedisConnection) setKey(key, value string) (string, error) {
 	ctx := context.Background()
-	SetResult := r.RedisClient.Set(ctx, key, value)
+	SetResult := r.RedisClient.Set(ctx, key, value, 0)
 	confirmation, err := SetResult.Result()
 	if err != nil {
-		return err
+		return "failed", err
 	} else {
-		return confirmation
+		return confirmation, nil
+	}
+}
+
+func (r RedisConnection) getValue(key string) (string, error) {
+	ctx := context.Background()
+	GetResult := r.RedisClient.Get(ctx, key)
+
+	if GetResult.Val() == "" {
+		return "", errors.New("key value not found in redis")
+	} else {
+		return GetResult.Val(), nil
+	}
+}
+
+func (r RedisConnection) isExist(key string) bool {
+	ctx := context.Background()
+	KeyExist, _ := r.RedisClient.Exists(ctx, key).Result()
+
+	if KeyExist == 1 {
+		return true
+	} else {
+		return false
 	}
 }
