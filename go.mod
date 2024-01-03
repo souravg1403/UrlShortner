36dcb37121d8e19290785ae29a80622ad1e99f52
Sourav Gupta <souravgmusic84@gmail.com>
2024-01-03 20:57:06 +0530

    Minor changes

--- go.mod
+++ go.mod
@@ -1,10 +1,13 @@
-module example.com/user/url-shortener
+module urlshortener
 
 go 1.21.1
 
 require (
+	github.com/lib/pq v1.10.9
+	github.com/redis/go-redis/v9 v9.3.0
+)
+
+require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/redis/go-redis/v9 v9.3.0 // indirect
 )
--- server.go
+++ server.go
@@ -2,28 +2,25 @@ package main
 
 //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
 import (
-	"./UrlShortner/helpers"
-	"encoding/json"
+	//"database/sql"
+
 	"fmt"
 	"log"
-	"math/rand"
+	"urlshortener/handlers"
+
+	_ "github.com/lib/pq"
+
+	//"log"
+
 	"net/http"
 	"os"
 	"os/exec"
-	"strings"
 	"sync"
-	"time"
 )
 
-type URL struct {
-	WebAddress string `json:"url"`
-}
-
-type shortURL struct {
-	ShortURL string `json:"shorturl"`
-}
-
 var port string = ":5000"
+
+// func (h *Handler)
 
 func servicesUp(wg *sync.WaitGroup) {
 
@@ -38,80 +35,15 @@ func servicesUp(wg *sync.WaitGroup) {
 	}
 }
 
-func getRandomString() string {
-	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
-	randomKey := make([]byte, 5)
-	for i := range randomKey {
-		randomKey[i] = charset[seededRand.Intn(len(charset))]
-	}
-	return string(randomKey)
-}
-
-func (redis *RedisConnection) urlShortening(u URL) string {
-	randomKey := getRandomString()
-	check := redis.isExist(randomKey)
-	for check {
-		randomKey = getRandomString()
-		check = redis.isExist(randomKey)
-	}
-
-	result, err := redis.setKey(randomKey, u.WebAddress, 0)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return "error"
-	} else {
-		fmt.Println(result)
-		return randomKey
-	}
-}
-
-func (redis *RedisConnection) GetUrl(w http.ResponseWriter, r *http.Request) {
-	var data URL
-	decoder := json.NewDecoder(r.Body)
-	myData := decoder.Decode(&data)
-	fmt.Println(myData)
-	if err := decoder.Decode(&data); err != nil {
-		fmt.Println(err)
-	}
-
-	value := redis.urlShortening(data)
-	shortenedUrl := "http://localhost" + port + "/" + value
-	shorturl := shortURL{
-		ShortURL: shortenedUrl,
-	}
-
-	dataBody, _ := json.Marshal(shorturl)
-	w.Write(dataBody)
-}
-
-func (redis *RedisConnection) FetchUrl(w http.ResponseWriter, r *http.Request) {
-
-	parts := strings.Split(r.URL.Path, "/")
-	if len(parts) >= 2 {
-		extractedPart := parts[1]
-		fmt.Println(extractedPart)
-		fmt.Printf("type: %T", extractedPart)
-
-		value, err := redis.getValue(extractedPart)
-		if err != nil {
-			fmt.Println(err)
-		}
-		http.Redirect(w, r, value, http.StatusFound)
-	} else {
-		http.NotFound(w, r)
-	}
-}
-
 func main() {
 	var wg sync.WaitGroup
-	handler := RedisConnectionHandler()
+	handler := handlers.NewHandler()
 	wg.Add(1)
 	go func() {
 
 		defer wg.Done()
 		mux := http.NewServeMux()
-		mux.HandleFunc("/get_url", handler.GetUrl)
+		mux.HandleFunc("/set", handler.SetURL)
 		mux.HandleFunc("/", handler.FetchUrl)
 
 		fmt.Println("Server is running on port " + port)
@@ -124,8 +56,14 @@ func main() {
 		log.Fatal(server.ListenAndServe())
 	}()
 
-	wg.Add(1)
-	go servicesUp(&wg)
+	// wg.Add(1)
+	// go servicesUp(&wg)
 
 	wg.Wait()
+
+	// postgresqlDbInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+	// 	"password=%s dbname=%s sslmode=disable",
+	// 	host, port, user, password, dbname)
+
+	// fmt.Println(postgresqlDbInfo)
 }
